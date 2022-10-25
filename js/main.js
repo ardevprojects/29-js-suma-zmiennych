adf8e15bb47e98ed6e97cc4c878a9c15d013f2a1
arudenko <rudandrzej@gmail.com>
2022-10-25 09:30:06 +0200

    frist commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 100;
+let secondNumber = 1200;
+
+function addNumbers(numberOne, numberTwo) {
+    let sum = numberOne + numberTwo;
+    return sum;
+}
+
+console.log( addNumbers(1, secondNumber) ); 
+let result = addNumbers(firstNumber, secondNumber);
+console.log(result);
